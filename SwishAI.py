982ed2520dffa3227b263bf6f86bc67b52421859
RaS-33 <rsarofiem@gmail.com>
2024-01-12 05:04:50 -0500

    FEATURE: part of tracking process completed

--- SwishAI.py
+++ SwishAI.py
@@ -16,11 +16,12 @@ mpipe_pose=mpipe.solutions.pose
 capture=cv2.VideoCapture(0) #setting up video capture device (webcam)
 ##!TODO: check if this is the right webcam input (now and when you get the temp webcam from walmart)
 
+
 #begin capturing when the webcam is running, stop when user exits
 while capture.isOpened():
     rt, frame = capture.read()
     cv2.imshow('SwishAI Feed', frame) # popup window
-    if cv2.waitKey(10) & 0xFF == ord('q'): #if the window is closed or "q" (for quit) is pressed, stop the loop
+    if cv2.waitKey(10) & 0xFF == ord('q'): #if "q" (for quit) is pressed, stop the loop
         break
 
 # exited webcam capturing, stop capturing and close the window
